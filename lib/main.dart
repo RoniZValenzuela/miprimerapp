3aa345c33e4207388c38dfdd7d0b7b9c137621af
Roni Zuniga <ronifzv@hotmail.com>
2023-10-27 12:15:16 -0700

    Cambio estilo centro 1

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,17 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyAppState extends ChangeNotifier{
+class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
   void getSiguiente() {
-    current = WordPair()
+    current = WordPair.random();
+    notifyListeners();
   }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +28,6 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
@@ -47,15 +46,46 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Idea aleatoria"),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(  
-            onPressed: print("Boton presionado");
-            child: Text("siguiente"))
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          
+            BigCard(idea: appState.current),
+            SizedBox(height: 20.0,),
+            ElevatedButton(
+                onPressed: () {
+                  appState.getSiguiente();
+                },
+                child: Text("Siguiente"))
+          ],
+        ),
       ),
     );
-  } 
+  }
+}
+
+class BigCard extends StatelessWidget {
+  final WordPair idea;
+
+  const BigCard({super.key, required this.idea});
+
+  @override
+  Widget build(BuildContext context) {
+    final tema = Theme.of(context);
+    final TextStyle = tema.textTheme.displayMedium!
+        .copyWith(color: tema.colorScheme.onPrimary);
+
+    return Card(
+      color: tema.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          idea.asLowerCase, 
+          style: TextStyle ,
+          semanticsLabel: "${idea.first} ${idea.second}",
+        )
+      ),
+    );
+  }
 }
