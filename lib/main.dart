8c6f13a5463f976666aca1d19dc583cafe1f0157
Roni Zuniga <ronifzv@hotmail.com>
2023-10-27 12:44:05 -0700

    Cambio de estilo 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:provider/provider.dart';
@@ -8,56 +6,86 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getSiguiente() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    print("algo");
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
           useMaterial3: true,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: const MyHomePage(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
 
-  final String title;
+  var favoritos = <WordPair>[];
+
+  void getSiguiente() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  void toggleFavorito() {
+    if (favoritos.contains(current)) {
+      favoritos.remove(current);
+    } else {
+      favoritos.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var idea = appState.current;
+    IconData icon;
+
+    if (appState.favoritos.contains(idea)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border_outlined;
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-          
             BigCard(idea: appState.current),
-            SizedBox(height: 20.0,),
-            ElevatedButton(
-                onPressed: () {
-                  appState.getSiguiente();
-                },
-                child: Text("Siguiente"))
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorito();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Me Gusta'),
+                ),
+                SizedBox(width: 10.0),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getSiguiente();
+                  },
+                  child: Text('Siguiente'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -67,13 +95,12 @@ class MyHomePage extends StatelessWidget {
 
 class BigCard extends StatelessWidget {
   final WordPair idea;
-
   const BigCard({super.key, required this.idea});
 
   @override
   Widget build(BuildContext context) {
     final tema = Theme.of(context);
-    final TextStyle = tema.textTheme.displayMedium!
+    final textStyle = tema.textTheme.displayMedium!
         .copyWith(color: tema.colorScheme.onPrimary);
 
     return Card(
@@ -81,10 +108,10 @@ class BigCard extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Text(
-          idea.asLowerCase, 
-          style: TextStyle ,
-          semanticsLabel: "${idea.first} ${idea.second}",
-        )
+          idea.asLowerCase,
+          style: textStyle,
+          semanticsLabel: "${idea.first} ${idea.second}", //se leera cla palabra
+        ),
       ),
     );
   }
